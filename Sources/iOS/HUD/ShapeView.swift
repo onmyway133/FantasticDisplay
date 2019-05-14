134cb40b312b188d460ee075cf446e4bc8a81e8a
Khoa Pham <onmyway133@gmail.com>
2019-05-14 14:51:41 +0200

    No animation in shape

--- Sources/iOS/HUD/ShapeView.swift
+++ Sources/iOS/HUD/ShapeView.swift
@@ -9,7 +9,6 @@ import UIKit
 
 public class ShapeView: UIView, AnimationAware {
     public let shapeLayer = CAShapeLayer()
-    public let animation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
 
     public var layerSize = CGSize(width: 80, height: 60)
     public var lineColor: UIColor = UIColor.darkGray
@@ -38,26 +37,6 @@ public class ShapeView: UIView, AnimationAware {
         shapeLayer.fillColor = lineColor.cgColor
         shapeLayer.strokeColor = lineColor.cgColor
         shapeLayer.lineWidth = 2
-
-        animation.timingFunction = CAMediaTimingFunction(name: .easeIn)
-        animation.fromValue = 0.0
-        animation.toValue = 1.0
-        animation.duration = duration
-    }
-
-    public override func didMoveToWindow() {
-        super.didMoveToWindow()
-
-        shapeLayer.strokeEnd = 0.0
-    }
-
-    public func startAnimation() {
-        shapeLayer.strokeEnd = 1.0
-        shapeLayer.add(animation, forKey: nil)
-    }
-
-    public func stopAnimation() {
-        shapeLayer.removeAllAnimations()
     }
 }
 
@@ -66,7 +45,7 @@ extension ShapeView {
         let path: String
         switch type {
         case .heart:
-            path = "M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181Z"
+            path = "M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"
         case .star:
             path = "M15.668 8.626l8.332 1.159-6.065 5.874 1.48 8.341-7.416-3.997-7.416 3.997 1.481-8.341-6.064-5.874 8.331-1.159 3.668-7.626 3.669 7.626zm-6.67.925l-6.818.948 4.963 4.807-1.212 6.825 6.068-3.271 6.069 3.271-1.212-6.826 4.964-4.806-6.819-.948-3.002-6.241-3.001 6.241z"
         case .custom(let string):
