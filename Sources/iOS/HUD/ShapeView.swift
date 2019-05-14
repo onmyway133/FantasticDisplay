12d1d995afcdf5c8afbd7ae9b1312f7f4f815de2
Khoa Pham <onmyway133@gmail.com>
2019-05-14 22:38:08 +0200

    Shift a bit

--- Sources/iOS/HUD/ShapeView.swift
+++ Sources/iOS/HUD/ShapeView.swift
@@ -44,6 +44,7 @@ public class ShapeView: UIView, AnimationAware {
 extension ShapeView {
     func apply(type: ShapeType) {
         let path: String
+        var shiftY: CGFloat = 1.0
         switch type {
         case .heart:
             path = """
@@ -54,6 +55,7 @@ extension ShapeView {
             """
         case .star:
             path = "M8 .2l4.9 15.2L0 6h16L3.1 15.4z"
+            shiftY = 10
         case .custom(let string):
             path = string
         }
@@ -61,9 +63,9 @@ extension ShapeView {
         let bezierPath = UIBezierPath(svgPath: path)
         let rect = bezierPath.bounds
         let scaleSize = CGSize(width: bezierPath.bounds.width/layerSize.width, height: bezierPath.bounds.height/layerSize.height)
-        let scale = max(scaleSize.width, scaleSize.height)
+        let scale = min(scaleSize.width, scaleSize.height)
 
-        bezierPath.apply(CGAffineTransform(translationX: -rect.origin.x, y: -rect.origin.y))
+        bezierPath.apply(CGAffineTransform(translationX: -rect.origin.x, y: -rect.origin.y * shiftY))
         bezierPath.apply(CGAffineTransform(scaleX: 1/scale, y: 1/scale))
 
         shapeLayer.path = bezierPath.cgPath
